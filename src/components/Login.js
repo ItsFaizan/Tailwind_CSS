6dd5f39578ad399dccd305ab496f47a41b0aa8c2
ItsFaizan <faizanjavid71@gmail.com>
2023-07-02 08:51:04 +0500

    Styling finalized using Tailwind CSS

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,27 +3,30 @@ import loginImg from '../assets/login.jpg'
 function Login()  {
 
         return (
-            <div className='grid grid-cols-1 sm:grid-cols-2'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 h-screen w-full'>
                 <div className='hidden sm:block'>
-                <img src={loginImg} alt="login" />
+                <img className='w-full h-full object-cover' src={loginImg} alt="login" />
                 </div>
 
-                <div>
-                    <form>
-                        <h2>SIGN I</h2>
-                        <div>
+                <div className='bg-gray-800 flex flex-col justify-center'>
+                    <form className='max-w-[400px] w-full mx-auto bg-gray-900 p-8 px-8 rounded-lg'>
+                        
+                    <h2 className="text-4xl text-white font-bold text-center">SIGN IN</h2>
+
+
+                        <div className='flex flex-col text-gray-400 py-2'>
                             <label>User Name</label>
-                            <input type="text" placeholder="User Name" />
+                            <input className='rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none' type="text" placeholder="User Name" />
                         </div>
-                        <div>
+                        <div className='flex flex-col text-gray-400 py-2'>
                             <label>Password</label>
-                            <input type="password" placeholder="Password" />
+                            <input className='rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none' type="password" placeholder="Password" />
                         </div>
-                        <div>
-                            <p><input type='checkbox'/> Remember Me</p>
+                        <div className='flex justify-between text-gray-400 py-2'>
+                            <p className='flex items-center'><input className='mr-2' type='checkbox'/> Remember Me</p>
                             <p>Forgot Password</p>
                         </div>
-                        <button type="submit">Sign In</button>
+                        <button className='w-full my-5 py-2 bg-teal-500 shadow-teal-500/50 hover:shadow-teal-500/40 text-white font-semibold rounded-lg ' type="submit">Sign In</button>
                     </form>
                 </div>
             </div>
